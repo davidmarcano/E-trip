aa14bba2d38d2d10381384cbdb973ac481487d8a
David Marcano <david.marcano@yale.edu>
2014-08-05 17:56:16 +0800

    adding working app

---
+++ New NEW/ETripWeb/Controllers/HomeController.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Senparc.Weixin.MP.AdvancedAPIs;
+using Senparc.Weixin.MP;
+using YiLuTong.BLL;
+using YiLuTong.Models;
+using System.Threading.Tasks;
+
+namespace YiLuTong.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        private string appId = "wxa6899f45ea1aaa1d";
+        private string secret = "f17531456e565f763511431cb1f3a170";
+
+        public ActionResult StartWay()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult AddWay(string fromLng, string fromLat, string toLng, string toLat, string userid)
+        {
+            var session = SessionHelper.Get(userid);
+            var user = session.UserInfo;
+            var routeModel = YiLuTong.BLL.Route.CreateRoute(fromLng, fromLat, toLng, toLat, user.openid, user.nickname, user.headimgurl);
+            session.Route = routeModel.Route;
+            Task.Factory.StartNew(() =>
+            {
+                var token = SeaStar.WechatAPI.TokenHandler.GetAccess_token(appId, secret);
+                SeaStar.WechatAPI.MessageHandle.SendText(token.access_token, new SeaStar.WechatAPI.Model.SendTextInfo()
+                {
+                    text = new SeaStar.WechatAPI.Model.Contents()
+                    {
+                        content = "恭喜您创建旅途成功，您的路线号码为：" + routeModel.RouteNumber + "，您只需将路线号码分享给您的好友后，便可以跟他们一起旅游了，赶快分享吧~"
+                    },
+                    touser = routeModel.UserId
+                });
+            });
+
+            return new JsonResult()
+            {
+                Data = routeModel,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+        public void deleteWay(string routenumber, string userid)
+        {
+            YiLuTong.BLL.Route.ExitRoute(routenumber, userid);
+        }
+
+
+        public ActionResult Search(string keyword, string userid)
+        {
+            ViewData["keyword"] = keyword;
+            ViewData["userid"] = userid;
+            return View();
+        }
+
+        public ActionResult Navigate(string title1, string userid, string lat, string lng)
+        {
+            ViewData["title1"] = title1;
+            ViewData["userid"] = userid;
+            ViewData["lat"] = lat;
+            ViewData["lng"] = lng;
+            return View();
+        }
+
+
+        [HttpPost]
+        public JsonResult JoinWay(FormCollection form)
+        {
+            var number = form["number"];
+            var userid = form["hidUserId"];
+            var lat = form["lat"];
+            var lng = form["lng"];
+
+            var session = SessionHelper.Get(userid);
+
+
+            if (number != null)
+            {
+
+                var reponse = YiLuTong.BLL.Route.JoinRoute(number, lat, lng, userid, session.UserInfo.nickname, session.UserInfo.headimgurl);
+                if (reponse.IsSuccess)
+                {
+                    session.Route = reponse.Route;
+                }
+
+               
+                return new JsonResult()
+                {
+                    Data = reponse
+                };
+
+            }
+
+            return new JsonResult();
+
+        }
+
+        [HttpGet]
+        public ActionResult Routing(string routnumber, string userid)
+        {
+            var number = routnumber;
+            var userId = userid;
+            //var number = Route.CreateRoute("108.88751", "34.230669", "109.069117", "34.276839", "oijfFtxWCUOtx5cXbF1pTIJMeVdo", "Jammey", "http://wx.qlogo.cn/mmopen/g3MonUZtNHkdmzicIlibx6iaFqAc56vxLSUfpb6n5WKSYVY0ChQKkiaJSgQ1dZuTOgvLLrhJbERQQ4eMsv84eavHiaiceqxibJxCfHe/46").RouteNumber;//form["routnumber"];
+            var model = YiLuTong.BLL.Route.GetRouteByNumber(number);
+            model.UserId = userId;
+            //var model = new YiLuTong.Models.Route();
+            //model.UserId = "111";
+            //model.RouteNumber = "111";
+            //model.ToLatitude = "34.230669";
+            //model.ToLongitude = "108.88751";
+            return View(model);
+        }
+
+        public JsonResult GetMembers(string number)
+        {
+            //var members = YiLuTong.BLL.Route.GetRouteMembersByNumber(number);
+            var members = new List<YiLuTong.Models.RouteMember>();
+            if (SessionHelper.Sessions == null)
+            {
+                SessionHelper.Sessions = new Dictionary<string, SessionInfo>();
+            }
+
+            foreach (var item in SessionHelper.Sessions.Keys)
+            {
+                var session = SessionHelper.Sessions[item];
+                if (session.Route != null && session.Route.RouteNumber == number && !string.IsNullOrEmpty(session.Latitude))
+                {
+                    members.Add(new YiLuTong.Models.RouteMember { UserId = session.UserInfo.openid, UserPicture = (session.UserInfo.headimgurl ?? string.Empty).Replace("/0", "/46"), UserName = session.UserInfo.nickname, CurrentLatitude = session.Latitude, CurrentLongitude = session.Longitude });
+                }
+            }
+            //var random = new Random();
+            //members.Add(new YiLuTong.Models.RouteMember { UserId = "111", UserName = "aaaa", CurrentLatitude = "34.230669", CurrentLongitude = (108 + random.Next(0, 20) / 100.00).ToString() });
+            //members.Add(new YiLuTong.Models.RouteMember { UserId = "222", UserName = "bbbb", CurrentLatitude = "34.230669", CurrentLongitude = (108 + random.Next(0, 20) / 100.00).ToString() });
+            //members.Add(new YiLuTong.Models.RouteMember { UserId = "333", UserName = "cccc", CurrentLatitude = "34.230669", CurrentLongitude = (108 + random.Next(0, 20) / 100.00).ToString() });
+            //members.Add(new YiLuTong.Models.RouteMember { UserId = "444", UserName = "dddd", CurrentLatitude = "34.230669", CurrentLongitude = (108 + random.Next(0, 20) / 100.00).ToString() });
+            //members.Add(new YiLuTong.Models.RouteMember { UserId = "555", UserName = "eee", CurrentLatitude = "34.230669", CurrentLongitude = (108 + random.Next(0, 20) / 100.00).ToString() });
+            //members.Add(new YiLuTong.Models.RouteMember { UserId = "666", UserName = "fff", CurrentLatitude = "34.230669", CurrentLongitude = (108 + random.Next(0, 20) / 100.00).ToString() });
+            return new JsonResult
+            {
+                Data = members,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+
+        public JsonResult SaveLocation(string lat, string lgd, string userId, string routNumber)
+        {
+            //YiLuTong.BLL.Route.RefrehLocation(userId, routNumber, lgd, lat);
+            return new JsonResult
+            {
+                Data = true,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+        
+
+        //public ActionResult CreateWay()
+        //{
+        //    return View();
+        //}
+
+        public JsonResult IamArrived(string userId)
+        {
+            YiLuTong.BLL.Route.IMArrived(userId);
+            return new JsonResult
+            {
+                Data = true,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+
+        public JsonResult IamHidden(string userId)
+        {
+            YiLuTong.BLL.Route.IMHidden(userId);
+            return new JsonResult
+            {
+                Data = true,
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Help()
+        {
+            return View();
+        }
+        //to handle the exiting routine
+        public ActionResult Endtrip(string routenumber, string userid)
+        {
+            ViewData["routenumber"] = routenumber;
+            ViewData["userid"] = userid;
+            return View();
+        }
+    }
+}
---
+++ New NEW/ETripWeb/Controllers/OAuth2Controller.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Senparc.Weixin.MP.AdvancedAPIs;
+using Senparc.Weixin.MP;
+using YiLuTong.BLL;
+using Senparc.Weixin.MP.Sample.CommonService.CustomMessageHandler;
+using System.IO;
+using Senparc.Weixin.MP.Entities;
+
+namespace YiLuTong.Controllers
+{
+    public class OAuth2Controller : Controller
+    {
+        //下面换成账号对应的信息，也可以放入web.config等地方方便配置和更换
+        private string appId = "wxa6899f45ea1aaa1d";
+        private string secret = "f17531456e565f763511431cb1f3a170";
+        private readonly string Token = "weixin";
+
+        public ActionResult Index()
+        {
+            //此页面引导用户点击授权
+            var url = OAuth.GetAuthorizeUrl(appId,
+                "http://123.138.75.236/OAuth2/IndexCallback", "JeffreySu", OAuthScope.snsapi_userinfo);
+            ViewData["Url"] = url;
+            Response.Redirect(url);
+            return View();
+        }
+        public ActionResult IndexCallback(string code, string state)
+        {
+            if (string.IsNullOrEmpty(code))
+            {
+                return Content("您拒绝了授权！");
+            }
+
+            if (state != "JeffreySu")
+            {
+                //这里的state其实是会暴露给客户端的，验证能力很弱，这里只是演示一下，实际上可以存任何想传递的数据，比如用户ID
+                return Content("验证失败！请从正规途径进入！");
+            }
+            //通过，用code换取access_token
+            var result = OAuth.GetAccessToken(appId, secret, code);
+            if (result.errcode != ReturnCode.请求成功)
+            {
+                return Content("错误：" + result.errmsg);
+            }
+
+            var useSession = SessionHelper.Get(result.openid);
+            var routeModel = new YiLuTong.Models.Route();
+            var userInfo = new WeixinUserInfoResult();
+            var webAccessToken = Untiy.GetAccessTokenFromCache(appId, secret);
+            userInfo = Senparc.Weixin.MP.CommonAPIs.CommonApi.GetUserInfo(webAccessToken, result.openid);
+            if (useSession != null)
+            {
+               
+                    useSession.UserInfo = userInfo;
+                
+
+                routeModel = Route.GetRouteByUserId(userInfo.openid);
+                useSession.Route = routeModel;
+            }
+            else
+            {
+                useSession = new SessionInfo();
+                useSession.UserInfo = userInfo;
+                routeModel = Route.GetRouteByUserId(userInfo.openid);
+                useSession.Route = routeModel;
+            }
+            
+            if (routeModel != null)
+                {
+                    return View("~/Views/Home/Routing.cshtml", routeModel);
+                }
+                else
+                {
+                    return View("~/Views/Home/CreateWay.cshtml", userInfo);
+                }
+        }
+
+        public ActionResult JoinWayCallback(string code, string state)
+        {
+            if (string.IsNullOrEmpty(code))
+            {
+                return Content("您拒绝了授权！");
+            }
+
+            if (state != "JeffreySu")
+            {
+                //这里的state其实是会暴露给客户端的，验证能力很弱，这里只是演示一下，实际上可以存任何想传递的数据，比如用户ID
+                return Content("验证失败！请从正规途径进入！");
+            }
+            //通过，用code换取access_token
+            var result = OAuth.GetAccessToken(appId, secret, code);
+            if (result.errcode != ReturnCode.请求成功)
+            {
+                return Content("错误：" + result.errmsg);
+            }
+
+            var useSession = SessionHelper.Get(result.openid);
+            var routeModel = new YiLuTong.Models.Route();
+            var userInfo = new WeixinUserInfoResult();
+
+            if (result.errcode != ReturnCode.请求成功)
+            {
+                return Content("错误：" + result.errmsg);
+            }
+
+            
+            var webAccessToken = Untiy.GetAccessTokenFromCache(appId, secret);
+            userInfo = Senparc.Weixin.MP.CommonAPIs.CommonApi.GetUserInfo(webAccessToken, result.openid);
+            if (useSession != null)
+            {
+
+                useSession.UserInfo = userInfo;
+
+
+                routeModel = Route.GetRouteByUserId(userInfo.openid);
+                useSession.Route = routeModel;
+            }
+            else
+            {
+                useSession = new SessionInfo();
+                useSession.UserInfo = userInfo;
+                routeModel = Route.GetRouteByUserId(userInfo.openid);
+                useSession.Route = routeModel;
+            }
+
+            if (routeModel != null)
+            {
+                return View("~/Views/Home/Endtrip.cshtml", routeModel);
+            }
+            else
+            {
+                return View("~/Views/Home/JoinWay.cshtml", userInfo);
+            }
+        }
+
+        public ActionResult JoinWay(string userId, string number)
+        {
+
+            if (!string.IsNullOrEmpty(userId) && !string.IsNullOrEmpty(number))
+            {
+                var webAccessToken = Untiy.GetAccessTokenFromCache(appId, secret);
+                var userInfo = Senparc.Weixin.MP.CommonAPIs.CommonApi.GetUserInfo(webAccessToken, userId);
+                var reponse = YiLuTong.BLL.Route.JoinRoute(number, "34", "180", userId, userInfo.nickname, userInfo.headimgurl);
+            }
+
+            var url = OAuth.GetAuthorizeUrl(appId,
+                "http://123.138.75.236/OAuth2/JoinWayCallback", "JeffreySu", OAuthScope.snsapi_userinfo);
+            ViewData["Url"] = url;
+            Response.Redirect(url);
+            return View();
+        }
+
+        public void Received()
+        {
+            string signature = Request["signature"];
+            string timestamp = Request["timestamp"];
+            string nonce = Request["nonce"];
+            string echostr = Request["echostr"];
+            if (Request.HttpMethod == "GET")
+            {
+                #region get method - 仅在微信后台填写URL验证时触发
+                if (CheckSignature.Check(signature, timestamp, nonce, Token))
+                {
+                    WriteContent(echostr); //返回随机字符串则表示验证通过
+                }
+                else
+                {
+                    WriteContent("failed:" + signature + "," + CheckSignature.GetSignature(timestamp, nonce, Token) + "。" +
+                                "如果你在浏览器中看到这句话，说明此地址可以被作为微信公众账号后台的Url，请注意保持Token一致。");
+                }
+                Response.End();
+                #endregion
+            }
+            else
+            {
+                //post method - 当有用户想公众账号发送消息时触发
+                if (!CheckSignature.Check(signature, timestamp, nonce, Token))
+                {
+                    WriteContent("参数错误！");
+                    return;
+                }
+                //v4.2.2之后的版本，可以设置每个人上下文消息储存的最大数量，防止内存占用过多，如果该参数小于等于0，则不限制
+                var maxRecordCount = 10;
+                //自定义MessageHandler，对微信请求的详细判断操作都在这里面。
+                var messageHandler = new CustomMessageHandler(Request.InputStream, maxRecordCount);
+                try
+                {
+                    //测试时可开启此记录，帮助跟踪数据
+                    messageHandler.RequestDocument.Save(Server.MapPath("~/App_Data/" + DateTime.Now.Ticks + "_Request_" + messageHandler.RequestMessage.FromUserName + ".txt"));
+                    //执行微信处理过程
+                    messageHandler.Execute();
+                    //测试时可开启，帮助跟踪数据
+                    messageHandler.ResponseDocument.Save(Server.MapPath("~/App_Data/" + DateTime.Now.Ticks + "_Response_" + messageHandler.ResponseMessage.ToUserName + ".txt"));
+                    WriteContent(messageHandler.ResponseDocument.ToString());
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    using (TextWriter tw = new StreamWriter(Server.MapPath("~/App_Data/Error_" + DateTime.Now.Ticks + ".txt")))
+                   {
+                        tw.WriteLine(ex.Message);
+                        tw.WriteLine(ex.InnerException.Message);
+                       if (messageHandler.ResponseDocument != null)
+                        {
+                            tw.WriteLine(messageHandler.ResponseDocument.ToString());
+                        }
+                        tw.Flush();
+                        tw.Close();
+                    }
+                    WriteContent("");
+                }
+                finally
+                {
+                    Response.End();
+                }
+            }
+        }
+
+        private void WriteContent(string str)
+        {
+            Response.Output.Write(str);
+        }
+    }
+}
